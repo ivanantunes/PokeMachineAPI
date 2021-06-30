744fc6e86a800aca6feea23af7e5554b1167a14d
Lucas Zaia <lucas.zaia30@gmail.com>
2021-06-30 15:23:26 -0300

    feito validçoes

--- src/main/java/com/pokemachine/api/proxy/Session.java
+++ src/main/java/com/pokemachine/api/proxy/Session.java
@@ -3,28 +3,42 @@ package com.pokemachine.api.proxy;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.crypto.EncryptedPrivateKeyInfo;
+
+import com.pokemachine.api.crud.AccountCrud;
+import com.pokemachine.api.crud.CashMachineCrud;
 import com.pokemachine.api.forms.FLogin;
 import com.pokemachine.api.interfaces.ProxyService;
+import com.pokemachine.api.models.MAccount;
+import com.pokemachine.api.models.MCashMachine;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 public class Session implements ProxyService {
-    
+
     /**
      * 
      */
     private List<FLogin> SESSIONS = new ArrayList<FLogin>();
 
+    private AccountCrud accountcrud = AccountCrud.getInstance();
+
+    private CashMachineCrud cashMachineCrud = CashMachineCrud.getInstance();
+
     /**
      * Instance of Class
      */
     private static Session instance;
-    
+
     /**
      * Constructor
      */
-    private Session() { }
+    private Session() {
+    }
 
     /**
      * Get Instance
+     * 
      * @return Instance of Class
      */
     public static Session getInstance() {
@@ -46,23 +60,53 @@ public class Session implements ProxyService {
         // TODO Auto-generated method stub
 
         // validar a conta
-        // validar a senha
-        // validar o caixa
-        
-        // validar se conta senha e caixa ja existem nas sessoes
-            // se exiter valide se os dados sao iguais
-                // se forem iguais passa
-                // se nao erro
-            // se nao existir passa pra gravar
-        
-        // alteraçoes dentro dos caixas
-            // status
+        List<MAccount> laccount = accountcrud.getAll(data.getCODE());
 
-        // salva nas sessoes 
+        if (laccount.size() >= 2) {
+            return false;
+        } else if (laccount.size() <= 0) {
+            return false;
+        }
+
+        String hashpassword = BCrypt.withDefaults().hashToString(12, data.getPASSWORD().toCharArray());
+
+        if (laccount.get(0).getACC_PASSWORD() != hashpassword) {
+
+            return false;
+        }
+
+        List<MCashMachine> lCashMachine = cashMachineCrud.getDataByID(data.getCASH_MACHINE_ID());
+
+        if (lCashMachine.size() >= 2) {
+            return false;
+        } else if (lCashMachine.size() <= 0) {
+            return false;
+        }
+
+        if (SESSIONS.contains(data)) {
+            if (SESSIONS.get(0).getCODE() == data.getCODE() && SESSIONS.get(0).getPASSWORD() == data.getPASSWORD()
+                    && SESSIONS.get(0).getCASH_MACHINE_ID() == data.getCASH_MACHINE_ID()) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            SESSIONS.add(data);
+        }
+        // se exiter valide se os dados sao iguais
+        // se forem iguais passa
+        // se nao erro
+        // se nao existir passa pra gravar
+
+        // alteraçoes dentro dos caixas
+        // status
+
+        // salva nas sessoes
 
         // retorna true
 
-        return false;
+        return true;
+
     }
 
     @Override
@@ -76,5 +120,5 @@ public class Session implements ProxyService {
         // TODO Auto-generated method stub
         return false;
     }
-  
+
 }
