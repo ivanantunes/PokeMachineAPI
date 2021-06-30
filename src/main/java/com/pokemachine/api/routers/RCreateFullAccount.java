2f63672807754335388d835213c8d70454d9bb0f
Lucas Zaia <lucas.zaia30@gmail.com>
2021-06-29 23:11:31 -0300

    resolvido

--- src/main/java/com/pokemachine/api/routers/RCreateFullAccount.java
+++ src/main/java/com/pokemachine/api/routers/RCreateFullAccount.java
@@ -20,7 +20,6 @@ import com.pokemachine.api.validators.FloatValidator;
 import com.pokemachine.api.validators.StringValidator;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
