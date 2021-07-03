c6f281dd66a8fb389ffea1637428206eb824ecf1
Lucas Zaia <lucas.zaia30@gmail.com>
2021-07-02 21:44:40 -0300

    cache session

---
+++ src/main/java/com/pokemachine/api/cache/CSession.java
@@ -0,0 +1,35 @@
+package com.pokemachine.api.cache;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.websocket.Session;
+
+import com.pokemachine.api.forms.FLogin;
+import com.pokemachine.api.interfaces.ProxyService;
+import com.pokemachine.api.models.MSession;
+
+public class CSession implements ProxyService{
+
+    private MSession session = MSession.Build();
+
+    private List<MSession> sessions = new ArrayList<MSession>();
+
+    @Override
+    public boolean startSession(FLogin data) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean authSession(FLogin data) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean endSession(FLogin data) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+}
