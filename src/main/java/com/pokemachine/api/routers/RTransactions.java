62e559238d92aa85c96282528213c997c91440b3
gbrextreme <gbrextreme@hotmail.com>
2021-07-12 20:44:37 -0300

    alteraçoes

--- src/main/java/com/pokemachine/api/routers/RTransactions.java
+++ src/main/java/com/pokemachine/api/routers/RTransactions.java
@@ -9,13 +9,18 @@ import com.pokemachine.api.forms.FDebit;
 import com.pokemachine.api.http.HttpMessage;
 import com.pokemachine.api.http.HttpResponse;
 import com.pokemachine.api.interfaces.RouterCrud;
+import com.pokemachine.api.models.MAccount;
 import com.pokemachine.api.models.MSession;
 import com.pokemachine.api.models.MTransferHistory;
+import com.pokemachine.api.utils.ProxySessionUtil;
+import com.pokemachine.api.validators.FloatValidator;
+import com.pokemachine.api.validators.StringValidator;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -56,13 +61,49 @@ public class RTransactions  {
 
     @CrossOrigin
     @PostMapping("/transfer/credit")
-    public ResponseEntity<HttpMessage> credit() {
+    public ResponseEntity<HttpMessage> credit(@RequestBody float value, @RequestHeader String token
+    ) {
         HttpMessage message = HttpMessage.build();
         int code = HttpResponse.UNAUTHORIZED;
         String validator = "";
         
-        
-        return null;
+        validator = StringValidator.isEmpty(token, "Token de Sessão");
+
+        if (!validator.isEmpty()) {
+            message.setCode(code).setMessage(validator).setError("");
+            return ResponseEntity.status(code).body(message);
+        }
+
+        validator = FloatValidator.isSmaller(value, 0, "Valor de Transferência");
+
+        if (!validator.isEmpty()) {
+            message.setCode(code).setMessage(validator).setError("");
+            return ResponseEntity.status(code).body(message);
+        }
+
+        try{
+            MSession session = MSession.Build().setSSI_TOKEN(token);
+
+            if (!ProxySessionUtil.getInstance().authSession(session)) {
+                code = HttpResponse.UNAUTHORIZED;
+                message.setCode(code).setMessage("Sessão invalida ou expirada.").setError("");
+                return ResponseEntity.status(code).body(message);
+            }
+            
+            MAccount account = ProxySessionUtil.getInstance()
+                .getAccountByToken(session);
+
+            account.setACC_BALANCE(account.getACC_BALANCE() + value);
+            accountCrud.update(account);
+
+            code = HttpResponse.OK;
+            message.setCode(code).setMessage("Valor de R$ " + value + " credita com sucesso.").setError("");
+            return ResponseEntity.status(code).body(message);
+        }catch (Exception e) {
+           code = HttpResponse.INTERNAL_SERVER_ERROR;
+           message.setCode(code).setMessage("Erro interno no servidor").setError(e.getMessage());
+           return ResponseEntity.status(code).body(message);      
+        }
     }
 
 }
