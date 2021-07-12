165f995d990ba91f05cde2e3ff57eb5941d0084d
Lucas Zaia <lucas.zaia30@gmail.com>
2021-07-12 20:39:26 -0300

    rota debit

--- src/main/java/com/pokemachine/api/routers/RTransactions.java
+++ src/main/java/com/pokemachine/api/routers/RTransactions.java
@@ -5,17 +5,22 @@ import java.util.List;
 import com.pokemachine.api.crud.AccountCrud;
 import com.pokemachine.api.crud.CashMachineCrud;
 import com.pokemachine.api.database.DBResult;
-import com.pokemachine.api.forms.FDebit;
+
 import com.pokemachine.api.http.HttpMessage;
 import com.pokemachine.api.http.HttpResponse;
 import com.pokemachine.api.interfaces.RouterCrud;
+import com.pokemachine.api.models.MAccount;
 import com.pokemachine.api.models.MSession;
 import com.pokemachine.api.models.MTransferHistory;
+import com.pokemachine.api.utils.ProxySessionUtil;
+import com.pokemachine.api.validators.FloatValidator;
+import com.pokemachine.api.validators.StringValidator;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -29,7 +34,7 @@ public class RTransactions  {
     /**
      * Cash Machine Crud
      */
-    private CashMachineCrud cashMachineCrud = CashMachineCrud.getInstance();
+     private CashMachineCrud cashMachineCrud = CashMachineCrud.getInstance();
 
     /**
      * Account Crud
@@ -37,14 +42,41 @@ public class RTransactions  {
     private AccountCrud accountCrud = AccountCrud.getInstance();
 
     @CrossOrigin
-    @PostMapping("debit")
-    public ResponseEntity<HttpMessage> debit(@RequestBody FDebit data){
+    @PostMapping("/transfer/debit")
+    public ResponseEntity<HttpMessage> debit(@RequestBody float value, 
+    @RequestHeader String token){
         HttpMessage message = HttpMessage.build();
         int code = HttpResponse.UNAUTHORIZED;
         String validator = "";
+
+        validator = StringValidator.isEmpty(token,"Token Session");
+
+        if(!validator.isEmpty()){
+            message.setCode(code).setMessage(validator).setError("");
+            return ResponseEntity.status(code).body(message);
+        }
+
+        validator = FloatValidator.isSmaller(value,0,"Valor Debitado");
+
+        if(!validator.isEmpty()){
+            message.setCode(code).setMessage(validator).setError("");
+            return ResponseEntity.status(code).body(message);  
+        }
         
         try{
+            
+            MSession session = MSession.Build().setSSI_TOKEN(token);
 
+            if (ProxySessionUtil.getInstance().authSession(session)){
+                code = HttpResponse.UNAUTHORIZED;
+                message.setCode(code).setMessage("Sessão Inválida ou Expirada").setError("");
+                return ResponseEntity.status(code).body(message);
+            }
+
+            MAccount account = ProxySessionUtil.getInstance().getAccountByToken(session);
+            
+            account.getACC_BALANCE();
+            
         }catch (Exception e) {
            code = HttpResponse.INTERNAL_SERVER_ERROR;
            message.setCode(code).setMessage("Erro interno no servidor").setError(e.getMessage());
